0d2862f0267b4a6256a73a62c6d5304c9c05af41
André Lucas Souza <souza.andr79@gmail.com>
2018-09-12 12:27:04 -0300

    fazendo o projeto

--- ProjetoAcademia/ProjetoAcademia/Properties/AssemblyInfo.cs
+++ ProjetoAcademia/ProjetoAcademia/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Informações gerais sobre um assembly são controladas através do seguinte 
-// conjunto de atributos. Altere o valor destes atributos para modificar a informação
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
 [assembly: AssemblyTitle("ProjetoAcademia")]
 [assembly: AssemblyDescription("")]
@@ -14,23 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Definir ComVisible como false torna os tipos neste assembly não visíveis 
-// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
 // COM, defina o atributo ComVisible como true nesse tipo.
 [assembly: ComVisible(false)]
 
-// O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
-[assembly: Guid("c89a5e0a-b207-419e-80df-d87de3fa9a22")]
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("abb07ae2-8d92-4fa1-a31b-36416531dc9d")]
 
-// Informações de Versão para um assembly consistem nos quatro valores a seguir:
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
 //      Versão Principal
 //      Versão Secundária 
 //      Número da Versão
 //      Revisão
 //
-// É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
-// utilizando o '*' como mostrado abaixo:
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- ProjetoAcademia/ProjetoAcademia/UI/Form1.Designer.cs
+++ ProjetoAcademia/ProjetoAcademia/UI/Form1.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Limpar os recursos que estão sendo usados.
         /// </summary>
-        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,16 +20,17 @@
             base.Dispose(disposing);
         }
 
-        #region código gerado pelo Windows Form Designer
+        #region Código gerado pelo Windows Form Designer
 
         /// <summary>
-        /// Método necessário para suporte do Designer - não modifique
+        /// Método necessário para suporte ao Designer - não modifique 
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
 
