0b686bfffc85b682e4d266bf858276c1ec54d496
André Lucas Souza <souza.andr79@gmail.com>
2018-09-12 12:39:07 -0300

    criada a classe de conexão

---
+++ ProjetoAcademia/ProjetoAcademia/DAL/Conexao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoAcademia.DAL
+{
+    class Conexao
+    {   //DESKTOP-81NJOA4
+        SqlConnection conexao = new SqlConnection(@"Data Source = .\sqlserver; Initial Catalog = ProjetoAcademia; Integrated Security = True;");
+
+        public SqlConnection Conectar()
+        {
+            if (conexao.State == ConnectionState.Closed)           
+                conexao.Open();            
+            return conexao;
+        }
+
+        public void Desconectar()
+        {
+            if (conexao.State == ConnectionState.Open)            
+                conexao.Close();            
+        }
+    }
+}
