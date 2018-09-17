4b775c4c5f182b30317cc0826510d7a622e854b1
André Lucas Souza <souza.andr79@gmail.com>
2018-09-17 16:58:13 -0300

    quase pronto
    quase tudo pronto

---
+++ ProjetoAcademia/ProjetoAcademia/BLL/Matricula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAcademia.BLL
+{
+    class Matricula
+    {
+        int CodMatricula, CodPlano, CodAluno;
+        String DataMatricula, DataVencimento;
+
+        public int CodMatricula1 { get => CodMatricula; set => CodMatricula = value; }
+        public int CodPlano1 { get => CodPlano; set => CodPlano = value; }
+        public int CodAluno1 { get => CodAluno; set => CodAluno = value; }
+        public string DataMatricula1 { get => DataMatricula; set => DataMatricula = value; }
+        public string DataVencimento1 { get => DataVencimento; set => DataVencimento = value; }
+    }
+}
---
+++ ProjetoAcademia/ProjetoAcademia/BLL/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAcademia.BLL
+{
+    class Usuario
+    {
+        int CodUsuario;
+	    String NomeUsuario, Senha, TipoUsuario;
+
+        public int CodUsuario1 { get => CodUsuario; set => CodUsuario = value; }
+        public string NomeUsuario1 { get => NomeUsuario; set => NomeUsuario = value; }
+        public string Senha1 { get => Senha; set => Senha = value; }
+        public string TipoUsuario1 { get => TipoUsuario; set => TipoUsuario = value; }
+    }
+}
